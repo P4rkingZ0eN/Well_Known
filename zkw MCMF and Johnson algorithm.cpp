2cd50450c810c27c2508d8bd96cd70bf607bdc21
Seojin Park <106161352+P4rkingZ0eN@users.noreply.github.com>
2023-01-23 14:46:25 +0900

    Update zkw MCMF and Johnson algorithm.cpp

--- zkw MCMF and Johnson algorithm.cpp
+++ zkw MCMF and Johnson algorithm.cpp
@@ -3,14 +3,14 @@
 #define se second
 using namespace std;
 priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > >pq;
-const int sz=805;
+const int sz=805,src=0,sink=801;
 int C[sz][sz],F[sz][sz],W[sz][sz],h[sz],dist[sz],L[sz];
 bool A[sz];
 vector<int>G[sz];
 void Johnson(){
     vector<bool>inq(sz,false);
-    fill(h,h+803,2e9);
-    fill(dist,dist+803,2e9);
+    fill(h,h+sz,2e9);
+    fill(dist,dist+sz,2e9);
     inq[0]=true;
     queue<int>q;q.push(0);
     while(!q.empty()){
@@ -27,7 +27,7 @@ void Johnson(){
             }
         }
     }
-    for(int i=0;i<=801;i++){
+    for(int i=0;i<sz;i++){
         for(int j=0;j<(int)G[i].size();j++){
             int k=G[i][j];
             if(C[i][k]>0)W[i][k]+=h[i]-h[k];
@@ -44,11 +44,11 @@ void Johnson(){
             pq.push({dist[nxt],nxt});
         }
     }
-    for(int i=0;i<=801;i++)dist[i]+=h[801]-h[0];
+    for(int i=0;i<sz;i++)dist[i]+=h[sink]-h[0];
 }
 bool upd(){
     int mn=2e9;
-    for(int i=0;i<=801;i++){
+    for(int i=0;i<sz;i++){
         if(!A[i])continue;
         for(int j=0;j<(int)G[i].size();j++){
             int k=G[i][j];
@@ -56,14 +56,14 @@ bool upd(){
         }
     }
     if(mn==2e9)return false;
-    for(int i=0;i<=801;i++){
+    for(int i=0;i<sz;i++){
         if(!A[i])dist[i]+=mn;
     }
     return true;
 }
 int dfs(int cur,int flow){
     A[cur]=true;
-    if(cur==801)return flow;
+    if(cur==sink)return flow;
     for(;L[cur]<(int)G[cur].size();L[cur]++){
         int nxt=G[cur][L[cur]];
         if(!A[nxt]&&dist[nxt]==dist[cur]+W[cur][nxt]&&C[cur][nxt]>0){
@@ -79,14 +79,14 @@ int dfs(int cur,int flow){
 int main(){
     int N,M;scanf("%d%d",&N,&M);
     for(int i=1;i<=N;i++){
-        C[0][i]=1;
-        G[0].push_back(i);
+        C[src][i]=1;
+        G[src].push_back(i);
         G[i].push_back(0);
     }
     for(int i=1;i<=M;i++){
-        C[i+400][801]=1;
-        G[801].push_back(i+400);
-        G[i+400].push_back(801);
+        C[i+400][sink]=1;
+        G[sink].push_back(i+400);
+        G[i+400].push_back(sink);
     }
     for(int i=1;i<=N;i++){
         int n;scanf("%d",&n);
@@ -106,9 +106,9 @@ int main(){
         memset(L,0,sizeof(L));
         int now=0;
         while(1){
-            now=dfs(0,2e9);
+            now=dfs(src,2e9);
             if(now==0)break;
-            res+=dist[801]*now;
+            res+=dist[sink]*now;
             cnt+=now;
             memset(A,false,sizeof(A));
         }
